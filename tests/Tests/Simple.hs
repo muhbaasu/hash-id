c600dac7950a8f93a99cbba3d489bffc32a3394b
Samuel Schuhmacher <forgetaboutit@muhbaasu.de>
2015-04-15 02:05:38 +0200

    Clean up tests

--- tests/Tests/Simple.hs
+++ tests/Tests/Simple.hs
@@ -9,32 +9,35 @@ import qualified Test.Framework                 as F
 import qualified Test.Framework.Providers.HUnit as F
 import           Test.HUnit
 
-consistent_shuffle_abc1 =
+consistentShuffleABC1 :: Assertion
+consistentShuffleABC1 =
   let shuffled = HI.consistentShuffle HI.defaultAlphabet "abc1"
   in "cUpI6isqCa0brWZnJA8wNTzDHEtLXOYgh5fQm2uRj4deM91oB7FkSGKxvyVP3l" @=? shuffled
 
-consistent_shuffle_abc2 =
+consistentShuffleABC2 :: Assertion
+consistentShuffleABC2 =
   let shuffled = HI.consistentShuffle HI.defaultAlphabet "abc2"
   in "tRvkhHx0ZefcF46YuaAqGLDKgM1W5Vp2T8n9s7BSoCjiQOdrEbJmUINywzXP3l" @=? shuffled
 
-encode_number = let options = HI.defaultOptions "this is my salt"
-                    encoder = HI.mkEncoder options
-                    hid = HI.encode encoder [12345]
-                    expected = "NkK9"
-                    result = HI.toText hid
-                in expected @=? result
-
-decode_number = let options = HI.defaultOptions "this is my salt"
-                    encoder = HI.mkEncoder options
-                    hid = fromJust $ HI.parse encoder "NkK9"
-                    expected = [12345]
-                    result = HI.decode encoder hid
-                in expected @=? result
+encodeNumber :: Assertion
+encodeNumber = let options = HI.defaultOptions "this is my salt"
+                   encoder = HI.mkEncoder options
+                   hid = HI.encode encoder [12345]
+                   expected = "NkK9"
+                   result = HI.toText hid
+               in expected @=? result
+decodeNumber :: Assertion
+decodeNumber = let options = HI.defaultOptions "this is my salt"
+                   encoder = HI.mkEncoder options
+                   hid = fromJust $ HI.parse encoder "NkK9"
+                   expected = [12345]
+                   result = HI.decode encoder hid
+               in expected @=? result
 
 tests :: F.Test
 tests =
   F.testGroup "Simple"
-  [ F.testCase "Encode number" encode_number
-  , F.testCase "Decode number" decode_number
-  , F.testCase "Shuffle with salt abc1" consistent_shuffle_abc1
-  , F.testCase "Shuffel with salt abc2" consistent_shuffle_abc2 ]
+  [ F.testCase "Encode number" encodeNumber
+  , F.testCase "Decode number" decodeNumber
+  , F.testCase "Shuffle with salt abc1" consistentShuffleABC1
+  , F.testCase "Shuffel with salt abc2" consistentShuffleABC2 ]
