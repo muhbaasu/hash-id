df543c381043d5f057a43450e9c29dd388b85d4c
Samuel Schuhmacher <forgetaboutit@muhbaasu.de>
2015-04-16 22:08:03 +0200

    Add inverse property tests

--- tests/Tests/Properties.hs
+++ tests/Tests/Properties.hs
@@ -10,14 +10,25 @@ import           Test.QuickCheck
 
 instance Arbitrary HI.HashEncoder where
   arbitrary = do
-   salt <- arbitrary
-   return $ HI.mkEncoder $ HI.defaultOptions salt
+    salt <- arbitrary
+    return $ HI.mkEncoder $ HI.defaultOptions salt
 
 prop_idempotent_encode :: HI.HashEncoder -> [Int] -> Bool
 prop_idempotent_encode enc nums = HI.encode enc nums == HI.encode enc nums
+
+decodeReversesEncode :: HI.HashEncoder -> [Int] -> Bool
+decodeReversesEncode enc nums = let encoded = HI.encode enc nums
+                                    decoded = HI.decode enc encoded
+                                in decoded == nums
+
+unhashReversesHash :: HI.Alphabet -> Int -> Bool
+unhashReversesHash alphabet num = let hashed = HI.hash alphabet num
+                                  in HI.unhash alphabet hashed == num
 
 tests :: F.Test
 tests =
   F.testGroup "Properties"
   [ F.testProperty "Idempotent encode" prop_idempotent_encode
+  , F.testProperty "decode reverses encode" decodeReversesEncode
+  , F.testProperty "unhash reverses hash" unhashReversesHash
   ]
